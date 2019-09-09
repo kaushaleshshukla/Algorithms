9545a712505f38fd978d2d79cf53213162fd38b4
kaushaleshshukla <45514816+kaushaleshshukla@users.noreply.github.com>
2019-09-09 16:42:42 +0530

    Update BellmanFords.cpp

--- Graph/BellmanFords.cpp
+++ Graph/BellmanFords.cpp
@@ -28,3 +28,36 @@ void BellmanFords(int source){
 }
 
 
+// Bellman Fords with path
+
+struct edge{
+    int u,v,cost;
+};
+
+vector<edge> edges;
+vector<int> dist;
+vector<int> par;
+int n,m;
+
+void BellmanFords(int source){
+    dist.clear();
+    dist.resize(n+1,INF);
+    par.clear();
+    par.resize(n+1,-1);
+    dist[source] = 0;
+    while(true){      
+        bool update = false;
+        for(auto e:edges){
+            if(dist[e.u]<INF){
+                if(dist[e.v]>dist[e.u]+e.cost){
+                    dist[e.v] = dist[e.u] + e.cost;
+                    par[e.v] = e.u;
+                    update = true;
+                }
+            }
+        }
+        if(!update)
+            break;
+    }
+}
+
