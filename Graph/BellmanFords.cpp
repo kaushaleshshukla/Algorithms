df8681a1cdd5a8a4cf1a87bf4a742a109051a7c0
kaushaleshshukla <45514816+kaushaleshshukla@users.noreply.github.com>
2019-09-09 16:53:28 +0530

    Update BellmanFords.cpp

--- Graph/BellmanFords.cpp
+++ Graph/BellmanFords.cpp
@@ -61,3 +61,52 @@ void BellmanFords(int source){
     }
 }
 
+// Finding negative cycle 
+
+struct edge{
+    int u,v,cost;
+};
+
+vector<edge> edges;
+vector<int> dist;
+vector<int> par;
+int n,m;
+
+vector<int> BellmanFords(int source){
+    dist.clear();
+    dist.resize(n+1,INF);
+    par.clear();
+    par.resize(n+1,-1);
+    dist[source] = 0;
+    int update;
+    for(int i=0;i<n;i++){      
+        update = -1;
+        for(auto e:edges){
+            if(dist[e.u]<INF){
+                if(dist[e.v]>dist[e.u]+e.cost){
+                    dist[e.v] = max(-INF, dist[e.u] + e.cost);
+                    par[e.v] = e.u;
+                    update = e.v;
+                }
+            }
+        }
+        if(update==-1)
+            break;
+    }
+    vector<int> cycle;
+    if(update==-1)
+        return cycle;
+
+    int y = update;
+    trace1(update);
+    for(int i=0;i<n;i++)
+        y = par[y];
+    int cur = y;
+    while(true){
+        cycle.push_back(cur);
+        if(cur==y && cycle.size()>1)
+            return cycle;
+         cur = par[cur];
+    }
+}
+
