f0b247b09a9618b475c15943d2797e76a21fa3c5
kaushaleshshukla <45514816+kaushaleshshukla@users.noreply.github.com>
2019-10-16 00:11:22 +0530

    Create Linear Diophantine.cpp

---
+++ Math/Linear Diophantine.cpp
@@ -0,0 +1,38 @@
+/* General form of Linear Diophantine equation : ax + by = c
+
+Generate all solution of equation by:
+      x = x0 + k*b/g
+      y = y0 - k*a/g
+      
+*/
+
+
+int gcd(int a, int b, int &x, int &y) {
+    if (a == 0) {
+        x = 0; y = 1;
+        return b;
+    }
+    int x1, y1;
+    int d = gcd(b%a, a, x1, y1);
+    x = y1 - (b / a) * x1;
+    y = x1;
+    return d;
+}
+
+bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {
+    g = gcd(abs(a), abs(b), x0, y0);
+    if (c % g) {
+        return false;
+    }
+
+    x0 *= c / g;
+    y0 *= c / g;
+    if (a < 0) x0 = -x0;
+    if (b < 0) y0 = -y0;
+    return true;
+}
+
+// Source : https://cp-algorithms.com/algebra/linear-diophantine-equation.html
+
+// Problem : https://codeforces.com/contest/633/problem/A
+// Solution : https://codeforces.com/contest/633/submission/62640906
